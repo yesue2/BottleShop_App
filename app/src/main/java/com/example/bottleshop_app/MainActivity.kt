c5c100d79f4500ecbc16bc75fc1f65ee01ee4dba
YeSeul <ysbsjh2464@naver.com>
2024-02-14 05:36:18 +0900

    feat: add bottom navigation bar and fragments on main screen

--- app/src/main/java/com/example/bottleshop_app/MainActivity.kt
+++ app/src/main/java/com/example/bottleshop_app/MainActivity.kt
@@ -2,10 +2,50 @@ package com.example.bottleshop_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bottleshop_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setFragment()
+
+        // 메인 화면 초기 실행 시 홈화면으로 설정
+        if (savedInstanceState == null) {
+            binding.menuNavi.selectedItemId = R.id.navi_home
+        }
+
+    }
+
+    private fun setFragment() {
+        binding.menuNavi.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navi_home -> {
+                    val fragmentTransaction = supportFragmentManager.beginTransaction()
+                    fragmentTransaction
+                        .replace(R.id.mainFrameLayout, HomeFragment())
+                        .commit()
+                    true
+                }
+                R.id.navi_chat -> {
+                    val fragmentTransaction = supportFragmentManager.beginTransaction()
+                    fragmentTransaction
+                        .replace(R.id.mainFrameLayout, ChatFragment())
+                        .commit()
+                    true
+                }
+                R.id.navi_info -> {
+                    val fragmentTransaction = supportFragmentManager.beginTransaction()
+                    fragmentTransaction
+                        .replace(R.id.mainFrameLayout, InfoFragment())
+                        .commit()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
